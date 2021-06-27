fe0c64e6865795411d644661fd3c1f5aa1ef6b64
harshaganesh100 <86505487+harshaganesh100@users.noreply.github.com>
2021-06-27 10:07:50 +0530

    Add files via upload

---
+++ Harsha.java
@@ -0,0 +1,10 @@
+class Harsha { 
+   int i=10;
+   static int y =15;
+    public static void main(String[] args) {
+      int i=9;
+     Harsha s=new Harsha();
+        System.out.println(i); 
+        System.out.println(s.i);
+    }
+}
